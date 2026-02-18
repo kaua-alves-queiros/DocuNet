20fd5c21dbcee6b7bdfdb2a6848502145dc46e37
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2026-02-18 19:07:35 -0400

    criado modelo base do usuario close #2

---
+++ DocuNet.Web/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DocuNet.Web.Models
+{
+    public class User : IdentityUser<Guid>
+    {
+        public User()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
