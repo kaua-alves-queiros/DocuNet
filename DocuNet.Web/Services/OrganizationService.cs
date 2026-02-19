448d269af2a3b41bade896b35a0c97e10a8f1ff4
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2026-02-19 01:35:04 -0400

    criado serviço de organização close #67

---
+++ DocuNet.Web/Services/OrganizationService.cs
@@ -0,0 +1,8 @@
+﻿using DocuNet.Web.Data;
+
+namespace DocuNet.Web.Services
+{
+    public class OrganizationService(ApplicationDatabaseContext context, ILogger<OrganizationService> logger)
+    {
+    }
+}
