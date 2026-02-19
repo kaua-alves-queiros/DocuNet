3b0090de56a3efcb098969a3f7c0647415fc7891
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2026-02-18 21:42:27 -0400

    criado constante de usuario padrão

---
+++ DocuNet.Web/Constants/DefaultUser.cs
@@ -0,0 +1,8 @@
+﻿namespace DocuNet.Web.Constants
+{
+    public class DefaultUser
+    {
+        public const string Email = "admin@xvideos.com";
+        public const string Password = "S3nh@Ultr@Fort3";
+    }
+}
