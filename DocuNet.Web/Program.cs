22abc55aa7ee8d49011701f09c15f0f892804ef1
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2026-02-18 21:44:17 -0400

    adicionado authentication, authorization e o uso do first setup close #5

--- DocuNet.Web/Program.cs
+++ DocuNet.Web/Program.cs
@@ -1,6 +1,10 @@
 using DocuNet.Web.Components;
+using DocuNet.Web.Constants;
 using DocuNet.Web.Data;
+using DocuNet.Web.Models;
 using DocuNet.Web.Services;
+using DocuNet.Web.Extensions;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -8,7 +12,7 @@ namespace DocuNet.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -17,11 +21,14 @@ namespace DocuNet.Web
                 .AddInteractiveServerComponents();
 
             builder.Host.UseSerilog((context, services, configuration) => configuration.ReadFrom.Configuration(context.Configuration).ReadFrom.Services(services));
-
+            builder.Services.AddCascadingAuthenticationState();
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("SystemAdmin", policy => policy.RequireRole("SystemAdmin"));
+            });
             builder.Services.AddDbContext<ApplicationDatabaseContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 
-            builder.Services.AddSingleton<UserService>();
-            
+            builder.Services.AddScoped<UserService>();
             
             var app = builder.Build();
 
@@ -29,20 +36,23 @@ namespace DocuNet.Web
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseAntiforgery();
 
             app.MapStaticAssets();
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode();
 
-            app.Run();
+            await app.InitializeDatabaseAsync();
+
+            await app.RunAsync();
         }
     }
 }
