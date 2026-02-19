dbf8736449dc98d279bbc24be7b4761670ee3b8e
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2026-02-18 20:45:36 -0400

    criado dto pra criar usuario #22

---
+++ DocuNet.Web/Dtos/User/CreateUserDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DocuNet.Web.Dtos.User
+{
+    /// <summary>
+    /// Objeto de transferência de dados para a criação de um novo usuário.
+    /// </summary>
+    /// <param name="CreatedBy">O identificador (GUID) do usuário ou administrador que está realizando a operação.</param>
+    /// <param name="Email">O endereço de e-mail que será utilizado como login e identidade única do novo usuário.</param>
+    /// <param name="Password">A senha de acesso, que deve seguir as políticas de complexidade do Identity.</param>
+    /// <param name="ConfirmPassword">A confirmação da senha. Deve ser idêntica ao campo <paramref name="Password"/>.</param>
+    public record CreateUserDto(
+        Guid CreatedBy,
+
+        [Required]
+        [EmailAddress]
+        string Email,
+
+        [Required]
+        string Password,
+
+        [Required]
+        [property: Compare("Password", ErrorMessage = "As senhas não conferem.")]
+        string ConfirmPassword
+    );
+}
