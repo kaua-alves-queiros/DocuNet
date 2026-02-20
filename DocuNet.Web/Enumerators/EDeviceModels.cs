90bab05eea203a968dc2073c532b0a301cba11e1
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2026-02-19 21:29:54 -0400

    criado tipos de dispositivos

---
+++ DocuNet.Web/Enumerators/EDeviceModels.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DocuNet.Web.Enumerators
+{
+    public enum EDeviceModels
+    {
+        [Display(Name = "Roteador")]
+        Router,
+
+        [Display(Name = "Switch")]
+        Switch,
+
+        [Display(Name = "Modem")]
+        Modem,
+
+        [Display(Name = "Servidor")]
+        Server,
+
+        [Display(Name = "Desktop / PC")]
+        PC,
+
+        [Display(Name = "Notebook")]
+        Notebook,
+
+        [Display(Name = "Ponto de Acesso (AP)")]
+        AccessPoint,
+
+        [Display(Name = "Roteador Wi-Fi")]
+        WifiRouter,
+
+        [Display(Name = "Especificações / Outros")]
+        Specs
+    }
+}
